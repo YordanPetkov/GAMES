24097099a85864534def2d2faa28cbe378bf1686
YordanPetkov <yordan_petkov180@abv.bg>
2019-04-15 23:01:46 +0300

    Draw sprites.

--- Jumping Pikachu/script/main.js
+++ Jumping Pikachu/script/main.js
@@ -1,39 +1,54 @@
 
 window.onload = function(){
+
     var playerCanvas = document.getElementById("player-canvas"),
     playerContex = playerCanvas.getContext("2d"),
     playerImg = document.getElementById("pikachu-sprite");
 
-    var frameIndex = 0,
-        framesCount = 4;
+    var pokeballImg = document.getElementById("pokeball-sprite");
 
+
+    var pikachuSprite = createSprite({
+        spritesheet: playerImg,
+        context: playerContex,
+        width: playerImg.width / 4,
+        height: playerImg.height,
+        numberOfFrames: 4,
+        loopTicksPerFrame: 5
+    });
+
+    var pikachuBody = createPhysicalBody({
+        coordinates: { x: 0, y: 0 },
+        speed: { x: 0, y: 0 },
+        height: pikachuSprite.height,
+        width: pikachuSprite.width
+    });
+
+    var pokeballSprite = createSprite({
+        spritesheet: pokeballImg,
+        context: playerContex,
+        width: pokeballImg.width / 18,
+        height: pokeballImg.height,
+        numberOfFrames: 18,
+        loopTicksPerFrame: 5
+    });
+
+
+    document.addEventListener('keydown', function(event){
+
+    });
+    
     function gameLoop() {
-        //clear previous frame
-        playerContex.clearRect(
-            0,
-            0,
-            playerImg.width / framesCount,
-            playerImg.height
-        );
-        //draw next frame
-        playerContex.drawImage(
-            playerImg,
-            frameIndex * playerImg.width / framesCount,
-            0,
-            playerImg.width / framesCount,
-            playerImg.height,
-            0,
-            0,
-            playerImg.width / framesCount,
-            playerImg.height
-        );
+        
+        var lastPikachuCoordinates = pikachuBody.move();
 
-        //update frame
-        frameIndex += 1;
+        pikachuSprite
+                .render(pikachuBody.coordinates, lastPikachuCoordinates)
+                .update();
 
-        if(frameIndex >= framesCount){
-            frameIndex = 0;
-        }
+        pokeballSprite
+                .render({x: 50, y: 60 }, {x: 50, y: 60 })
+                .update();
 
         window.requestAnimationFrame(gameLoop);
     }
---
+++ Jumping Pikachu/script/physical-body.js
@@ -0,0 +1,30 @@
+function createPhysicalBody(options){
+    "use strict";
+
+    function move() {
+        var self = this;
+
+        var lastCoordinates = JSON.parse(JSON.stringify(self.coordinates));
+        //var lastCoordinates = { x: self.coordinates.x, y: self.coordinates.y};
+        self.coordinates.x += self.speed.x;
+        self.coordinates.y += self.speed.y;
+
+        return lastCoordinates;
+        
+    }
+
+    function collidesWith(otherPhysicalBody){
+        
+    }
+
+    var physicalBody = {
+        coordinates: options.coordinates,
+        speed: options.speed,
+        height: options.height,
+        width: options.width,
+        move: move,
+        collidesWith: collidesWith
+    };
+
+    return physicalBody;
+}
